bc8cc88b64a0540e6cb192084b18428e890f0fb5
CallyCa <blackyrite@hotmail.com>
2020-12-26 10:59:53 -0300

    Add: create database photos and config models photos | create config db .sh

--- Install.sh
+++ Install.sh
@@ -6,3 +6,12 @@ sudo apt-get update
 
 echo '# Upgrade package list'
 sudo apt-get upgrade -y
+
+# Config Database Students
+
+npx sequelize migration:create --name=students
+npx sequelize db:migrate
+
+# Config Database Users
+npx sequelize migration:create --name=users
+npx sequelize db:migrate
